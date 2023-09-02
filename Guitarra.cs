c771445c002e0891ade23e3ca437724941c4f336
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-01 23:35:07 -0300

    classe Guitarra criada e codificada

---
+++ Guitarra.cs
@@ -0,0 +1,21 @@
+﻿namespace Biblioteca;
+
+public class Guitarra : InstrumentoMusical, IAfinavel
+{
+    public Guitarra(string nome, string tipo) : base(nome, tipo) { }
+
+    public void Afinar()
+    {
+        Console.WriteLine($"Afinando {Nome}..");
+    }
+
+    public bool VerificarAfinacao()
+    {
+        return true;
+    }
+
+    public override void Tocar()
+    {
+        Console.WriteLine($"Tocando {Nome}..");
+    }
+}
