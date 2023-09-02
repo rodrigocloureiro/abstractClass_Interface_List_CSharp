108e6777d8af580137dd4ccd51709e4ad5a43010
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-01 23:37:21 -0300

    classe Triangulo criada e codificada

---
+++ Triangulo.cs
@@ -0,0 +1,11 @@
+﻿namespace Biblioteca;
+
+public class Triangulo : InstrumentoMusical
+{
+    public Triangulo(string nome, string tipo) : base(nome, tipo) { }
+
+    public override void Tocar()
+    {
+        Console.WriteLine($"Tocando {Nome}..");
+    }
+}
