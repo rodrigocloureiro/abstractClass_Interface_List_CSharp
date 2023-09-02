766c8f5029fb1dcb448ee43a18cdb2d5464ced7a
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-01 23:29:18 -0300

    classe Violao criada e codificada

---
+++ Violao.cs
@@ -0,0 +1,21 @@
+﻿namespace Biblioteca;
+
+public class Violao : InstrumentoMusical, IAfinavel
+{
+    public Violao(string nome, string tipo) : base(nome, tipo) { }
+
+    public void Afinar()
+    {
+        Console.WriteLine($"Afinando {Nome}..");
+    }
+
+    public bool VerificarAfinacao()
+    {
+        return true;
+    }
+
+    public override void Tocar()
+    {
+        Console.WriteLine($"Tocando ${Nome}..");
+    }
+}
