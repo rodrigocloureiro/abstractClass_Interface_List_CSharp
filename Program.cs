8c2e6ceff1f0fc1117e1894b00e7d4b8ec621b50
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-01 23:42:32 -0300

    instancias de instrumentos adicionadas na lista

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        List<InstrumentoMusical> listaInsturmentos = new List<InstrumentoMusical>();
+        IList<InstrumentoMusical> listaInsturmentos = new List<InstrumentoMusical>();
+        listaInsturmentos.Add(new Violao("Violão", "Corda"));
+        listaInsturmentos.Add(new Baixo("Baixo", "Corda"));
+        listaInsturmentos.Add(new Guitarra("Guitarra", "Corda"));
+        listaInsturmentos.Add(new Triangulo("Triângulo", "Percussão"));
     }
 }
