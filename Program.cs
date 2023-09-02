beae33a9ee742a91e5709db83bcf21391b1fa40f
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-01 23:39:02 -0300

    lista de instrumentos criada e implementada

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿namespace Biblioteca;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<InstrumentoMusical> listaInsturmentos = new List<InstrumentoMusical>();
+    }
+}
