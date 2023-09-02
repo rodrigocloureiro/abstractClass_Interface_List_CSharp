64294a0a9b225f33c42b1747f0a025b1633ca1f1
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-01 23:31:16 -0300

    classe Baixo criada e codificada

---
+++ Baixo.cs
@@ -0,0 +1,21 @@
+﻿namespace Biblioteca;
+
+public class Baixo : InstrumentoMusical, IAfinavel
+{
+    public Baixo(string nome, string tipo) : base(nome, tipo) { }
+
+    public void Afinar()
+    {
+        Console.WriteLine($"Afinando {Nome}..");
+    }
+
+    public bool VerificarAfinacao()
+    {
+        return false;
+    }
+
+    public override void Tocar()
+    {
+        Console.WriteLine($"Tocando {Nome}..");
+    }
+}
